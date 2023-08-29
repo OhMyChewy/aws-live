3881a8b46abf038fc65a0947115f0f899fdcec9c
OhMyChewy <99484236+OhMyChewy@users.noreply.github.com>
2023-08-29 15:40:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c0e1h94tmafp.us-east-1.rds.amazonaws.com"
+customuser = "asw_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "neojunlin-employee"
+customregion = "us-east-1"
+
